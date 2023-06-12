3abb58b2128b369d59e00dc56d59469c0d4a1f80
Addika1630 <addika1630@gmail.com>
2023-06-12 04:24:45 -0700

    Javascript

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,3 @@
 #!/usr/bin/node
-const argsCount = process.argv.length - 2;
-if (argsCount === 0) {
-  console.log("No argument");
-} else if (argsCount === 1) {
-  console.log("Argument found");
-} else {
-  console.log("Arguments found");
-}
+const count = process.argv.length;
+console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
