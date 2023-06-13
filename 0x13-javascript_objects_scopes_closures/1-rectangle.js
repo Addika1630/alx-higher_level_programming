f46b9feadc33e178027ae3a73be2de3fb393bc51
Addika1630 <addika1630@gmail.com>
2023-06-13 13:53:35 -0700

    JavaScript

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+    constructor (width, height) {
+      this.width = width;
+      this.height = height;
+    }
+  };
---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+    constructor (width, height) {
+      if (typeof width === 'number' && typeof height === 'number' && width > 0 && height > 0) {
+        this.width = width;
+        this.height = height;
+      }
+    }
+  };
