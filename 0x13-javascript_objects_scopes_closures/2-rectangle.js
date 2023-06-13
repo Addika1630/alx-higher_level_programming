f4947a2dc116f1f077add5e65225f029ca84b0d4
Addika1630 <addika1630@gmail.com>
2023-06-13 14:06:28 -0700

    JavaScirpt

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,6 +2,7 @@
 
 // Checks for Rectangle parameters
 
+
 module.exports = class Rectangle {
     constructor (width, height) {
       if (typeof width === 'number' && typeof height === 'number' && width > 0 && height > 0) {
