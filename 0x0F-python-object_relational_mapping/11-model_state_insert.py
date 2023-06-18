ec7163d27c3358cc70d04ef9b5acd84228026fd0
Addika1630 <addika1630@gmail.com>
2023-06-18 14:02:37 -0700

    ORM

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,26 +1,37 @@
 #!/usr/bin/python3
 """
-prints the State object with the name passed as
-argument from the database hbtn_0e_6_usa"""
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-from model_state import Base, State
+Adds the State object “Louisiana” to
+the database hbtn_0e_6_usa
+"""
 
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    engine = create_engine(
-                            'mysql+mysqldb://{}:{}@localhost/{}'
-                            .format(
-                                        sys.argv[1],
-                                        sys.argv[2],
-                                        sys.argv[3]
-                                            ),
-                            pool_pre_ping=True
-                                )
-    session = Session(engine)
-    new_State = State(name="Louisiana")
-    re = session.add(new_State)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    # Initialize engine
+    Base.metadata.create_all(engine)
+
+    # Initialize session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Add new record
+    new_state = State(name="Louisiana")
+    session.add(new_state)
     session.commit()
-    print(new_State.id)
+
+    # Query
+    query = session.query(State).\
+        order_by(State.id.desc()).first()
+
+    # Print query
+    print("{}".format(query.id))
+
+    # Close session
     session.close()
--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,26 +1,33 @@
 #!/usr/bin/python3
 """
-deletes all State objects with a name containing
-the letter a from the database hbtn_0e_6_usa"""
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-from model_state import Base, State
+deletes all State objects with a name containing the
+letter 'a' from the database hbtn_0e_6_usa
+"""
 
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    engine = create_engine(
-                            'mysql+mysqldb://{}:{}@localhost/{}'
-                            .format(
-                                        sys.argv[1],
-                                        sys.argv[2],
-                                        sys.argv[3]
-                                            ),
-                            pool_pre_ping=True
-                                )
-    session = Session(engine)
-    re = session.query(State).filter(State.name.like('%a%'))
-    for row in re:
-        session.delete(row)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    # Initialize engine
+    Base.metadata.create_all(engine)
+
+    # Initialize session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Update record
+    record = session.query(State).\
+        filter(State.name.like('%a%')).all()
+    for delete_record in record:
+        session.delete(delete_record)
+
     session.commit()
+
+    # Close session
     session.close()
